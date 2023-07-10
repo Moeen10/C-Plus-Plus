e1f2f645af671efc3a4d6c4d5934a8b9fd6ec0b8
Moeen10 <62027833+Moeen10@users.noreply.github.com>
2023-07-10 13:47:03 +0600

    count Node + count leaf

---
+++ binary_tree_input_&_countNode.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+        int val;
+        Node * left;
+        Node * right;
+
+        Node(int v)
+        {
+            this->val = v;
+            this->left = nullptr;
+            this->right = nullptr;
+        }
+};
+
+Node * input_tree()
+{
+    int val;
+    cin>>val;
+    Node * root;
+    if(val==-1)
+    {
+        root=nullptr;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+    queue<Node*>q;
+    if(root)q.push(root);
+    while(!q.empty())
+    {
+        //1.Ber kore ana
+
+        Node * f = q.front();
+        q.pop();
+
+        //2.Kaj kora
+        int l,r;
+        cin>>l>>r;
+        Node *Cleft;
+        Node *Cright;
+        if(l==-1){
+            Cleft = nullptr;
+        }
+        else{
+            Cleft = new Node(l);
+        }
+        if(r==-1){
+            Cright = nullptr;
+        }
+        else{
+            Cright = new Node(r);
+        }
+        f->left=Cleft;
+        f->right = Cright;
+
+        //3.Chilfren gulake dhukaw
+         if(f->left)q.push(f->left);
+         if(f->right)q.push(f->right);
+    }
+     return root;
+}
+
+
+int count(Node * root)
+{
+    if(root==nullptr)return 0;
+    int l = count(root->left);
+    int r = count(root->right);
+
+    return l+r+1;
+}
+
+int main()
+{
+    Node * root =input_tree();
+    cout<<"Count tree node: ";
+    cout<<count(root);
+
+}
